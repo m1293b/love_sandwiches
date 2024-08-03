d6a9d2f52cb5db329bb66c6339ce1f61944b01c0
Máté Balla <m1293b@gmail.com>
2024-08-03 20:24:58 +0000

    Code tidied up

--- run.py
+++ run.py
@@ -63,10 +63,16 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated succesfully.\n")
 
-data = get_sales_data()
 
-sales_data = [int(num) for num in data]
+def main():
+    
+    """Run all program functions"""
+    
+    data = get_sales_data()
 
-update_sales_worksheet(sales_data)
+    sales_data = [int(num) for num in data]
 
-print(sales_data)
+    update_sales_worksheet(sales_data)
+  
+print("Welcome to Love Sandwiches Data Automation")  
+main()
